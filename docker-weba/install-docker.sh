ad1c168c69e2f72d9a18412957cfd434b93f5b41
liumapp <liumapp.com@gmail.com>
2018-03-06 15:11:49 +0800

    if found target folder , than rm it

---
+++ docker-weba/install-docker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+title=docker-weba
+PROJECT_NAME=docker-weba
+IMAGE_NAME=docker-weba
+
+MACHINE_PORT=2222
+CONTAINER_PORT=2222
+
+if [ ! -d "target" ]; then
+    echo 'find target folder , ready to rm'
+    rm -rf "${PROJECT_NAME}/target"
+fi
+
+
+
