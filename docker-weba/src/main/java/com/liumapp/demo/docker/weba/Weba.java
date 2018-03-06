183067ebe2e1251783926daab77e307bb6f25ff8
liumapp <liumapp.com@gmail.com>
2018-03-06 09:00:31 +0800

    add one project

---
+++ docker-weba/src/main/java/com/liumapp/demo/docker/weba/Weba.java
@@ -0,0 +1,18 @@
+package com.liumapp.demo.docker.weba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by liumapp on 3/6/18 8:59 AM.
+ * E-mail:liumapp.com@gmail.com
+ * home-page:http://www.liumapp.com
+ */
+@SpringBootApplication
+public class Weba {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Weba.class , args);
+    }
+
+}
---
+++ docker-weba/src/main/java/com/liumapp/demo/docker/weba/controller/IndexController.java
@@ -0,0 +1,21 @@
+package com.liumapp.demo.docker.weba.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by liumapp on 9/28/17.
+ * E-mail:liumapp.com@gmail.com
+ * home-page:http://www.liumapp.com
+ */
+@RestController
+@RequestMapping("/")
+public class IndexController {
+
+    @GetMapping("/")
+    public String index () {
+        return "success";
+    }
+
+}
