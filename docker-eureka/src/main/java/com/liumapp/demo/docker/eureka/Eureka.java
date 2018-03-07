2934e600272f756ad09dc8a5838cc0258347b2d5
liumapp <liumapp.com@gmail.com>
2018-03-07 10:25:45 +0800

    conf eureka

--- docker-eureka/src/main/java/com/liumapp/demo/docker/eureka/Eureka.java
+++ docker-eureka/src/main/java/com/liumapp/demo/docker/eureka/Eureka.java
@@ -2,17 +2,19 @@ package com.liumapp.demo.docker.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
- * Created by liumapp on 9/28/17.
+ * Created by liumapp on 3/7/18 10:24 AM.
  * E-mail:liumapp.com@gmail.com
  * home-page:http://www.liumapp.com
  */
-@SpringBootApplication(scanBasePackages = {"com.liumapp"})
-public class Main {
+@EnableEurekaServer
+@SpringBootApplication
+public class Eureka {
 
     public static void main(String[] args) {
-        SpringApplication.run(Main.class , args);
+        SpringApplication.run(Eureka.class , args);
     }
 
 }
