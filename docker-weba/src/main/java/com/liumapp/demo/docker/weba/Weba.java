b00cbfd6b3cff295b7fa5f10d3afc2695527f490
liumapp <liumapp.com@gmail.com>
2018-03-07 10:29:42 +0800

    add eureka client dependency of weba

--- docker-weba/src/main/java/com/liumapp/demo/docker/weba/Weba.java
+++ docker-weba/src/main/java/com/liumapp/demo/docker/weba/Weba.java
@@ -2,12 +2,14 @@ package com.liumapp.demo.docker.weba;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Created by liumapp on 3/6/18 8:59 AM.
  * E-mail:liumapp.com@gmail.com
  * home-page:http://www.liumapp.com
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Weba {
 
