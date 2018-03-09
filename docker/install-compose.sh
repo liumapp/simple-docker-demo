53c25ad604f6af57e84148c2000b0241b7e293a0
liumapp <liumapp.com@gmail.com>
2018-03-09 10:27:29 +0800

    install docker-compose:1.19.0

--- docker/install-compose.sh
+++ docker/install-compose.sh
@@ -12,3 +12,7 @@ echo '$                                                           $'
 echo '============================================================='
 echo '.'
 
+curl -L "https://github.com/docker/compose/download/1.19.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+chmod +x /usr/local/bin/docker-compose
+
